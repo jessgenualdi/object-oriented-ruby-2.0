abb7f18ec0d4eff35e4ab5799b9327bcf5ae67d8
Jessica Genualdi <jessi.genualdi@fcb.com>
2016-10-31 21:12:58 -0500

    created new method, full-name

---
+++ employee.rb
@@ -0,0 +1,56 @@
+# # Arrays
+# employee1 = ["Majora", "Carter", 80000, true]
+# employee2 = ["Danilo", "Campos", 70000, true]
+# puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
+# puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
+
+# # Hashes
+# employee1 = {"first_name" => "Majora", "last_name" => "Carter", "salary" => 80000, "active" => true}
+# employee2 = {"first_name" => "Danilo", "last_name" => "Campos", "salary" => 70000, "active" => true}
+# puts "#{employee1['first_name']} #{employee1['last_name']} makes #{employee1['salary']} a year."
+# puts "#{employee2['first_name']} #{employee2['last_name']} makes #{employee2['salary']} a year."
+
+# # Hashes using symbols for keys
+# employee1 = {:first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true}
+# employee2 = {first_name: "Danilo", last_name: "Campos", salary: 70000, active: true}
+# puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
+# puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
+
+# Instances of a class
+class Employee
+  attr_reader :first_name, :last_name
+
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @status = input_options[:status]
+  end
+
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+  end
+
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+
+  def full_name
+    if @last_name[-1] == "s"
+      puts "#{@first_name} #{@last_name}, Esquire"
+    else
+      puts "#{@first_name} #{@last_name}"
+    end
+  end
+end
+
+employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
+employee2 = Employee.new(first_name: "Danilo", last_name: "Campos", salary: 70000, active: true)
+employee1.print_info
+employee2.print_info
+employee2.give_annual_raise
+employee2.print_info
+
+puts employee1.full_name
+puts employee2.full_name
+
